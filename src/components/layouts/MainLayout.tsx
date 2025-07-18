1b804dec5d8763fc6efa682085e75f7a8aa746ea
phuoc <connguaho@gmail.com>
2025-07-18 12:22:57 +0700

    feat: add Tailwind CSS support and new layout components
    - Updated package.json to include Tailwind CSS and react-router-dom dependencies.
    - Created MainLayout and Navbar components for consistent layout structure.
    - Added Footer component placeholder for future use.
    - Implemented Home page component.
    - Set up routing with AppRoutes for Home page.
    - Introduced mock data files for categories and products.
    - Configured TypeScript paths for easier imports.
    - Updated Vite configuration to resolve paths and include Tailwind CSS plugin.

---
+++ src/components/layouts/MainLayout.tsx
@@ -0,0 +1,32 @@
+import React, { type ReactNode } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+// import Navbar from "./Navbar";
+// import Footer from "./Footer"; 
+
+interface MainLayoutProps {
+  children?: ReactNode;
+}
+
+const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+  const location = useLocation();
+  const hideFooterPaths = ['/login', '/register', '/cart'];
+  const shouldHideFooter = hideFooterPaths.includes(location.pathname);
+
+  return (
+    <div className="relative flex flex-col min-h-screen w-full">
+      {/* <Navbar /> */}
+      <div className="relative w-full min-h-screen bg-[#FFEFEC]">
+        <div className=" inset-0 min-h-screen">
+          {children || <Outlet />}
+        </div>
+        {!shouldHideFooter && (
+          <div>
+            {/* <Footer /> */}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MainLayout;
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import MainLayout from "@/components/layouts/MainLayout";
+// import Courses from "@/pages/Courses";
+
+const AppRoutes: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        {/* <Route path="/courses" element={<Courses />} /> */}
+      </Route>
+    </Routes>
+  );  
+};
+
+export default AppRoutes;
